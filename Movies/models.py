ad070864e1097ea346857280293d23c6ab9185fd
jn-mic <jackotienokey@gmail.com>
2021-07-08 15:51:22 +0300

    creating a user model

--- Movies/models.py
+++ Movies/models.py
@@ -1,3 +1,13 @@
+from os import name
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+CHO
+
+class customUser(AbstractUser):
+    profiles=models.ManyToManyField('profiles',null=True,blank=True)
+
+class profile=(models.Model):
+    name=models.CharField(max_length=300)
+    age_limit=models.CharField(max_length=20,choices=c)
